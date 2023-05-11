50da42df5eb9d8015fa02e7444fd6814db386ac9
ivanmalyshev <ivanmalyshev1994@gmail.com>
2023-05-11 16:56:11 +0300

    first

---
+++ README.md
@@ -0,0 +1,3 @@
+запуск bash post_send.sh <адрес ящика> <количество писем>
+
+пример bash post_send.sh mid@icmail.ru 10
---
+++ post_send.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+a=$2
+i=1
+date
+while [[ i -le $a ]]; do
+#sleep 3
+cat message | mail -s "Theme Welcome" $1
+i=$(($i+1))
+echo "_____________________________________________________"
+done
+
